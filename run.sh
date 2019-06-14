70bb0a011f2f171555837387aae74c9681e8905a
jsarnik <jsarnik-a@jsarnik-l1.local>
2019-06-14 16:44:28 -0400

    build-run scripts

---
+++ run.sh
@@ -0,0 +1,4 @@
+forever stop 0
+NODE_ENV=development forever start server.js
+cd react-app
+npm start
---
+++ setup_run.sh
@@ -0,0 +1,8 @@
+npm i 
+npm install forever -g
+forever stop 0
+wait
+NODE_ENV=development forever start server.js
+cd react-app
+npm i
+npm start
