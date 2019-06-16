f725f0a14b19bfd8059aee8875c8a3d953b28825
jsarnik <jsarnik-a@jsarnik-l1.local>
2019-06-16 19:00:09 -0400

    build files

---
+++ react-app/build/precache-manifest.14720ed55d6bb47c5d6fd40ebe548fc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8d8481037e8c547919efda6fde7f840",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e263e4f5c6a71cecca6",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9b45cad8d5733f86f519",
+    "url": "/static/css/main.22036577.chunk.css"
+  },
+  {
+    "revision": "2e263e4f5c6a71cecca6",
+    "url": "/static/js/2.0bb7a9ca.chunk.js"
+  },
+  {
+    "revision": "9b45cad8d5733f86f519",
+    "url": "/static/js/main.38c130ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
